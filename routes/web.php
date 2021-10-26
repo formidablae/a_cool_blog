5a53bf32facfe4e3fbe86271885faf80cb7473b7
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-26 23:15:00 +0200

    Created routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
@@ -13,6 +17,24 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/users', UserController::class . '@getAllUsers');
+$router->get('/user/{id}', UserController::class . '@getUser');
+$router->post('/user/new', UserController::class . '@newUser');
+$router->delete('/user/{id}/delete', UserController::class . '@deleteUser');
+$router->get('/user/{user_id}/posts', PostController::class . '@getAllPostsOfAUser');
+$router->get('/user/{user_id}/comments', PostController::class . '@getAllCommentsOfAUser');
+
+$router->get('/posts', PostController::class . '@getAllPosts');
+$router->get('/post/{id}', PostController::class . '@getPost');
+$router->post('/post/{user_id}/new', PostController::class . '@newPost');
+$router->post('/post/{post_id}/edit', PostController::class . '@editPost');
+$router->delete('/post/{post_id}/delete', PostController::class . '@deletePost');
+$router->delete('/posts/{user_id}/delete', PostController::class . '@deleteAllPostsOfAUser');
+
+$router->get('/comments', CommentController::class . '@getAllComments');
+$router->get('/comment/{id}', CommentController::class . '@getComment');
+$router->post('/comment/{user_id}/{post_id}/new', PostController::class . '@newComment');
+$router->post('/comment/{comment_id}/new', CommentController::class . '@editComment');
+$router->delete('/comment/{comment_id}/delete', CommentController::class . '@deleteComment');
+$router->delete('/comments/{post_id}/delete', CommentController::class . '@deleteAllCommentsOfAPost');
+$router->delete('/comments/{user_id}/delete', CommentController::class . '@deleteAllCommentsOfAUser');
