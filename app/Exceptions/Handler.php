c8f2dab98d9dd6aaa2b79bc714bf30b503e7cb6c
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-28 01:20:35 +0200

    Definded some eexception handling with error codes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,15 @@
 
 namespace App\Exceptions;
 
+use ErrorException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
+use UnexpectedValueException;
 
 class Handler extends ExceptionHandler
 {
@@ -47,8 +50,21 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
-    {
+    public function render($request, Throwable $exception) {
         return parent::render($request, $exception);
+        if ($exception instanceof AuthorizationException) {
+            return response('Unauthorized.', 401);
+        } else if ($exception instanceof ModelNotFoundException) {
+            return response('Resource not found.', 404);
+        } else if ($exception instanceof MethodNotAllowedHttpException) {
+            return response('Method not allowed.', 405);
+        } else if ($exception instanceof HttpException) {
+            return response('Http error.', 409);
+        } else if ($exception instanceof UnexpectedValueException) {
+            return response('Unexpected value.', 422);
+        } else if ($exception instanceof ErrorException) {
+            return response('Request error.', 422);
+        }
+        return parent::render($request, $exception);  // other errors, error code 500
     }
 }
