012fff7953281dfc0b37eacecdad019259b75482
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-28 00:52:22 +0200

    Grouped some of the endpoints defined

--- routes/web.php
+++ routes/web.php
@@ -20,25 +20,30 @@ use App\Http\Controllers\CommentController;
 /**
  * Posts endpoints
  */
-$router->get('/posts', PostController::class . '@getAllPosts');  // required endpoint
-$router->get('/posts/{post_id}', PostController::class . '@getPost');  // required endpoint
-$router->post('/posts', PostController::class . '@newPost');  // required endpoint
-$router->put('/posts/{post_id}', PostController::class . '@editPost');  // required endpoint
-$router->delete('/posts/{post_id}', PostController::class . '@deletePost');  // required endpoint
+$router->group(['prefix' => 'posts'], function () use ($router) {
+    $router->get('/', PostController::class . '@getAllPosts');  // required endpoint
+    $router->get('/{post_id}', PostController::class . '@getPost');  // required endpoint
+    $router->post('/', PostController::class . '@newPost');  // required endpoint
+    $router->put('/{post_id}', PostController::class . '@editPost');  // required endpoint
+    $router->delete('/{post_id}', PostController::class . '@deletePost');  // required endpoint
+});
 $router->get('/users/{user_id}/posts', PostController::class . '@getAllPostsOfAUser');
 $router->delete('/users/{user_id}/posts', PostController::class . '@deleteAllPostsOfAUser');
 
 /**
  * Comments endpoints
  */
+$router->group(['prefix' => 'comments'], function () use ($router) {
+    $router->get('/{comment_id}', CommentController::class . '@getComment');  // required endpoint
+    $router->post('/', CommentController::class . '@newComment');  // required endpoint
+    $router->put('/{comment_id}', CommentController::class . '@editComment');  // required endpoint
+    $router->delete('/{comment_id}', CommentController::class . '@deleteComment');  // required endpoint
+    $router->get('/', CommentController::class . '@getAllComments');
+    
+});
 $router->get('/posts/{post_id}/comments', CommentController::class . '@getAllCommentsOfAPost');  // required endpoint
-$router->get('/comments/{comment_id}', CommentController::class . '@getComment');  // required endpoint
-$router->post('/comments', CommentController::class . '@newComment');  // required endpoint
-$router->put('/comments/{comment_id}', CommentController::class . '@editComment');  // required endpoint
-$router->delete('/comments/{comment_id}', CommentController::class . '@deleteComment');  // required endpoint
 $router->get('/users/{user_id}/comments', CommentController::class . '@getAllCommentsOfAUser');
 $router->delete('/user/{user_id}/comments', CommentController::class . '@deleteAllCommentsOfAUser');
-//$router->get('/comments', CommentController::class . '@getAllComments');
 $router->delete('/posts/{post_id}/comments', CommentController::class . '@deleteAllCommentsOfAPost');
 
 
@@ -47,7 +52,9 @@ $router->delete('/posts/{post_id}/comments', CommentController::class . '@delete
  */
 $router->post('/auth/register', UserController::class . '@newUser');  // required endpoint
 $router->post('/auth/login', UserController::class . '@loginUser');  // required endpoint
-$router->get('/users', UserController::class . '@getAllUsers');
-$router->get('/users/{user_id}', UserController::class . '@getUser');
-$router->put('/users/{user_id}', UserController::class . '@editUser');
-$router->delete('/users/{user_id}', UserController::class . '@deleteUser');
+$router->group(['prefix' => 'users'], function () use ($router) {
+    $router->get('/', UserController::class . '@getAllUsers');
+    $router->get('/{user_id}', UserController::class . '@getUser');
+    $router->put('/{user_id}', UserController::class . '@editUser');
+    $router->delete('/{user_id}', UserController::class . '@deleteUser');
+});
