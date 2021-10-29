54e5ce49621eff7e66fbbf5334464f85dbf9e0cc
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-29 19:59:41 +0200

    Removed unused use statements in controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Traits\UserIdTrait;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Traits\UserIdTrait;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
